4113578e301c09067232ce5f0a3b3ec8edd1c176
0xkowalskidev <0xkowalskidev@gmail.com>
2025-06-21 13:36:16 +0100

    Added send command script to docker image

---
+++ images/minecraft/send-command.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Check if a command was passed 
+if [ -z "$1" ]; then
+  echo "Usage: $0 <command>"
+  exit 1
+fi
+
+PIPE_PATH="/tmp/command-fifo"
+
+# Send command to the pipe
+echo "$1" > "$PIPE_PATH"
--- images/minecraft/start.sh
+++ images/minecraft/start.sh
@@ -6,5 +6,9 @@ cd /data/server
 # Create EULA file based on environment variable
 echo "eula=${EULA}" > eula.txt
 
-# Start server with configurable memory
-exec java -Xmx${MEMORY_MB}M -Xms${MEMORY_MB}M -jar server.jar nogui
+# Create named pipe for command input
+PIPE_PATH="/tmp/command-fifo"
+mkfifo "$PIPE_PATH"
+
+# Start server with configurable memory, using named pipe as input
+exec java -Xmx${MEMORY_MB}M -Xms${MEMORY_MB}M -jar server.jar nogui < "$PIPE_PATH"
