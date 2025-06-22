583fa09363759716ac5482aef4feee4adbefca84
0xkowalskidev <0xkowalskidev@gmail.com>
2025-06-22 21:35:07 +0100

    Added server properties to minecraft with default settings and configurable options, changed workflow to ignore images thatdont need building

--- images/minecraft/start.sh
+++ images/minecraft/start.sh
@@ -6,6 +6,27 @@ cd /data/server
 # Create EULA file based on environment variable
 echo "eula=${EULA}" > eula.txt
 
+# Update server.properties with environment variables
+if [ ! -z "$SERVER_NAME" ]; then
+    sed -i "s/motd=.*/motd=${SERVER_NAME}/" server.properties
+fi
+
+if [ ! -z "$MOTD" ]; then
+    sed -i "s/motd=.*/motd=${MOTD}/" server.properties
+fi
+
+if [ ! -z "$DIFFICULTY" ]; then
+    sed -i "s/difficulty=.*/difficulty=${DIFFICULTY}/" server.properties
+fi
+
+if [ ! -z "$GAMEMODE" ]; then
+    sed -i "s/gamemode=.*/gamemode=${GAMEMODE}/" server.properties
+fi
+
+# Set consistent ports (server port should match container port)
+sed -i "s/server-port=.*/server-port=25565/" server.properties
+sed -i "s/rcon.port=.*/rcon.port=25575/" server.properties
+
 # Create named pipe for command input
 PIPE_PATH="/tmp/command-fifo"
 mkfifo "$PIPE_PATH"
